55c8d7957da9c08ff76da5db7e4c52bae2f873da
Seth Daniel Lawson Goldman <seth.daniel.goldman@gmail.com>
2016-07-01 10:12:03 -0400

    First commit, pushing to master.

---
+++ authorization_code/test.js
@@ -0,0 +1,6 @@
+var Spotify = require('spotify-web-api-js');
+var s = new Spotify();
+
+var spotifyApi = new SpotifyWebApi();
+
+spotifyApi.setAccessToken('BQCQpNgflALIoedR7i_ALgw3bsFfvCTTg18VncBdgU4aShVfLDmy2Fnm1THA_ZWBF508YalEd2u689G605wNpIW9am6rqbMz9WY2WDZtCGTV_5_zUcIHCPHjn5jAAmBUdtu3G_8WZZuZtDO0UruPvvaN4yywJQDQ6rc');
